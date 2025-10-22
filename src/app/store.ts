b75915359a03463dab4fecb1f2cc99948bd0652b
ammmar7874-dev <ammmar7874@gmail.com>
2025-10-22 23:35:51 +0500

    new

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,20 +4,22 @@ import storage from "redux-persist/lib/storage";
 import favoritesReducer from "./favoritesSlice";
 
 const persistConfig = {
-    key: "root",
-    storage
+  key: "root",
+  storage,
 };
 
 const combinedReducers = combineReducers({ favorites: favoritesReducer });
 const persistedReducers = persistReducer(persistConfig, combinedReducers);
 
 export const store = configureStore({
-    reducer: persistedReducers,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: false
-    })
+  reducer: persistedReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+//heredfhdgfhdf
